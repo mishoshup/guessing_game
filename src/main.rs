71897768859d0de7083cf430dcbc3f979cee5e3a
Danial Haikal <202131799+mishoshup@users.noreply.github.com>
2025-12-11 13:13:40 +0800

    feat(comparing): enable comparing and handle all case
    - Need to handle case where input is not a number.

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,35 @@
+use rand::Rng;
+use std::cmp::Ordering;
 use std::io;
 
 fn main() {
     println!("Guess the number!");
 
-    println!("Please input your guess.");
+    let secret_number = rand::thread_rng().gen_range(1..=100);
 
-    let mut guess = String::new();
+    loop {
+        println!("Please input your guess.");
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
+        let mut guess = String::new();
 
-    println!("You guessed: {guess}");
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
+        let guess: u32 = guess.trim().parse().expect("Please type a number");
+
+        println!("You guessed: {guess}");
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Equal => {
+                println!("You got it right!");
+                break;
+            }
+            Ordering::Greater => println!("Too big!"),
+        }
+    }
+    // if (guess == secret_number) {
+    //     println!("You Win")
+    // }
 }
